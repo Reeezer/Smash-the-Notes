0655b4bc434f07b3fc79a5bd6a0e5d48bde9b048
Luca Davide Meyer <luca.meyer@he-arc.ch>
2021-01-12 10:20:18 +0100

    Update gamewindow.cpp

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -4,7 +4,7 @@
 
 GameWindow::GameWindow(QWidget *parent)
     : QStackedWidget(parent)
-{   
+{
     setWindowTitle("Smash The Notes");
     Game *game = new Game();
     GameView *gameView = new GameView(game, this);
