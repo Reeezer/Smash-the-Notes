06c75a767e5b3d3d93fa29c127ebac21b4f94eac
Millet Ethan <ethan.millet@he-arc.ch>
2021-01-15 15:13:23 +0100

    ajout du bouton et fonction mainsettings vers mainmenu et fonction mainsettings vers controlsettings

--- mainsettings.cpp
+++ mainsettings.cpp
@@ -7,18 +7,29 @@ MainSettings::MainSettings(Game *game, QWidget *parent)
     inputButton = new QPushButton(this);
     volumeSlider = new QSlider(Qt::Horizontal, this);
     pathWidget = new PathWidget(this);
+    returnButton = new QPushButton(this);
 
-    QFormLayout *layout = new QFormLayout(this);
+    QFormLayout *layout = new QFormLayout();
     layout->addRow(tr("delay"), delaySpinBox);
     layout->addRow(tr("controls"), inputButton);
     layout->addRow(tr("volume"), volumeSlider);
     layout->addRow(tr("musics directory"), pathWidget);
+
+    QHBoxLayout *spacerLayout = new QHBoxLayout();
+    spacerLayout->addWidget(returnButton, 1);
+    spacerLayout->addStretch(9);
+
+    QVBoxLayout *returnButtonLayout = new QVBoxLayout();
+    returnButtonLayout->addLayout(spacerLayout);
+    returnButtonLayout->addLayout(layout);
+    this->setLayout(returnButtonLayout);
 
     connect(delaySpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, &MainSettings::setNewDelay);
     connect(inputButton, &QPushButton::clicked, this, &MainSettings::displayControlSettings);
     connect(volumeSlider, &QSlider::valueChanged, this, &MainSettings::setNewVolume);
     connect(pathWidget->mapPathText, &QLineEdit::textChanged, this, &MainSettings::validateAndSetNewDirectory);
     connect(pathWidget->mapPathButton, &QPushButton::clicked, this, &MainSettings::openModalFileExplorer);
+    connect(returnButton, &QPushButton::clicked, this, &MainSettings::returnButtonPressed);
 }
 
 void MainSettings::setNewDelay()
@@ -28,7 +39,7 @@ void MainSettings::setNewDelay()
 
 void MainSettings::displayControlSettings()
 {
-
+    emit controlSettingsCall();
 }
 
 void MainSettings::setNewVolume()
@@ -44,4 +55,9 @@ void MainSettings::validateAndSetNewDirectory()
 void MainSettings::openModalFileExplorer()
 {
 
+}
+
+void MainSettings::returnButtonPressed()
+{
+    emit mainMenuCall();
 }
