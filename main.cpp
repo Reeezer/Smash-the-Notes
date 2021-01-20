9e06bf082c4444ba95cc99dc99fc1d51bb7f3eb6
Millet Ethan <ethan.millet@he-arc.ch>
2021-01-20 16:15:04 +0100

    changement d'apparence des boutons suivant leur état, fond du menu principal

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     GameWindow w;
     w.show();
-                      //EndScreen { background-color: black; }
+
     a.setStyleSheet("* { font-family: \"Foo\"; font-size: 32px; }\
                     QWidget { background-color : DarkOliveGreen; }\
                     QPushButton { color: white; background-color: DarkGreen; border-radius: 4px; border: 2px solid black; }\
-                    QListWidget { background-color : OliveDrab; }");
+                    QListWidget { background-color : OliveDrab; }\
+                    QPushButton:hover { border: 3px solid lightBlue; }\
+                    QPushButton:disabled { background-color: grey; color: lightgrey; }\
+                    ");
 
     return a.exec();
 }
